66e7dfd71ee1ee0c0ccf415b6408b276cece9ec8
Let-cal <letrancatlam123@gmail.com>
2025-01-03 15:42:23 +0700

    fix bug deloy vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
-import * as path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          recharts: ['recharts'],
+          lodash: ['lodash'],
+          'lottie-web': ['lottie-web'],
+          ui: ['@radix-ui/react-accordion', '@radix-ui/react-alert-dialog', '@radix-ui/react-checkbox', '@radix-ui/react-dialog']
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['rollup']
+  }
+})
